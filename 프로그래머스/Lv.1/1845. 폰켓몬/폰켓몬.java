a93eee6c5fca9a46b74f5e4f6b5a46e0d6b19cc4
Jihyun Han <startingfrom33@gmail.com>
2024-01-15 16:11:56 +0900

    [level Lv.1] Title: 폰켓몬, Time: 2.15 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/Lv.1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/Lv.1/1845. 폰켓몬/폰켓몬.java
@@ -1,27 +1,22 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] nums) {
-		int size = nums.length;
-		int max = 0;
-		int answer = 0;
-		
-		
-		for(int i=0; i<size-1; i++) {
-			for(int j=0; j<size-1-i; j++) {
-				if(nums[i] == nums[i+j+1])
-					nums[i+j+1] = 0;
-			}//for j ends
-		}//for i ends
-		
-		for(int i=0; i<size; i++)
-			if(nums[i] != 0)
-				max++;
-		
-		if(size/2 < max)
-			answer = size/2;
-		else if(size/2 == max)
-			answer = max;
-		else
-			answer = max;
+        int answer = 0;
+        List<Integer> myPkm = new ArrayList<>();
+        
+        for(int n:nums){
+            if(!myPkm.contains(n)){
+                    myPkm.add(n);
+                    continue;
+                }
+            }
+        
+        
+        if(myPkm.size() > nums.length/2)
+            answer = nums.length/2;
+        else
+            answer = myPkm.size();
         return answer;
     }
 }
