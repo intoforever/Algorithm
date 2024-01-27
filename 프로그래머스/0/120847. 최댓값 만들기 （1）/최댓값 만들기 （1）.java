16f0b4983154aceba5976bef8b63d82f67d0282a
Jihyun Han <startingfrom33@gmail.com>
2024-01-27 21:27:43 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.33 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,14 +1,18 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
         
-        for(int i=0; i<numbers.length-1; i++)
-            for(int j=0; j<numbers.length-1-i; j++){
-                if(numbers[j] > numbers[j+1]){
-                    int temp = numbers[j];
-                    numbers[j] = numbers[j+1];
-                    numbers[j+1] = temp;
-                }
-            }
+        // for(int i=0; i<numbers.length-1; i++)
+        //     for(int j=0; j<numbers.length-1-i; j++){
+        //         if(numbers[j] > numbers[j+1]){
+        //             int temp = numbers[j];
+        //             numbers[j] = numbers[j+1];
+        //             numbers[j+1] = temp;
+        //         }
+        //     }
+        
+        Arrays.sort(numbers);
         
         int answer = numbers[numbers.length-1] * numbers[numbers.length-2];        
         return answer;
