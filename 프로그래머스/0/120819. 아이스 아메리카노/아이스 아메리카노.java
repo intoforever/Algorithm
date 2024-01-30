a07eacd2341f4eee15cac8fd6e8f43b239e0b54a
Jihyun Han <startingfrom33@gmail.com>
2024-01-30 20:42:05 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,9 +1,20 @@
 class Solution {
     public int[] solution(int money) {
         int[] answer = new int[2];
+        int coffeePrice = 5500;
         
-        answer[0] = money/5500;
-        answer[1] = money%5500;
+        for(int i=0; i<answer.length; i++){
+            if(i == 0){
+                answer[i] = money/coffeePrice;
+                continue;
+            }
+            
+            answer[i] = money%coffeePrice;
+            break;
+        }
+        
+        // answer[0] = money/5500;
+        // answer[1] = money%5500;
         
         return answer;
     }
