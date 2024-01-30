10b443b1345baa10434a499c3cc5dff36d8f9f99
Jihyun Han <startingfrom33@gmail.com>
2024-01-30 20:54:51 +0900

    [level 0] Title: 순서쌍의 개수, Time: 5.72 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,16 +1,11 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
         
         /* 약수 구하기 */
-        List<Integer> list = new ArrayList<>();
         for(int i=1; i<=n; i++)        
             if(n%i == 0)
-                list.add(i);
-        
-        answer = list.size();
+                answer++;
         
         return answer;
     }
