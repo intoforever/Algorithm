5c3de2513149b77f5239da19185df430a4732831
Jihyun Han <startingfrom33@gmail.com>
2024-02-01 16:58:14 +0900

    [level 0] Title: 모음 제거, Time: 0.09 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        String[] vowels = {"a", "e", "i", "o", "u"};
+        
+        StringBuilder sb = new StringBuilder();
+        
+        main:
+        for(int i=0; i<my_string.length(); i++){
+            for(String v:vowels)
+                if(String.valueOf(my_string.charAt(i)).equals(v)){
+                    sb.append("");
+                    continue main;
+                }
+            sb.append(String.valueOf(my_string.charAt(i)));
+        }  
+        
+        return sb.toString();
+    }
+}
