4546ece60644909475fe412afe3780ebfac5a277
Jihyun Han <startingfrom33@gmail.com>
2024-02-01 17:52:42 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.14 ms, Memory: 65.9 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,18 +1,12 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        
         String stringN = String.valueOf(n);
         String[] stringArr = stringN.split("");
-        List<Integer> intList = new ArrayList<>();
         
-        for(String s:stringArr){
-            intList.add(Integer.parseInt(s));
-        }
-        
-        for(int i=0; i<intList.size(); i++)
-            answer += intList.get(i);
+        for(int i=0; i<stringArr.length; i++)
+            answer += Integer.parseInt(stringArr[i]);
 
         return answer;
     }
