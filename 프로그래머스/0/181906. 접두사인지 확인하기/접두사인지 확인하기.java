cd40eb6a139c9abc6397487f648d51906d64483e
Jihyun Han <startingfrom33@gmail.com>
2024-02-10 00:16:03 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(String my_string, String is_prefix) {
         int answer = 0;
         
-        if(my_string.indexOf(is_prefix) == 0 && my_string.contains(is_prefix))
+        if(my_string.startsWith(is_prefix))
             answer = 1;
         
         return answer;
