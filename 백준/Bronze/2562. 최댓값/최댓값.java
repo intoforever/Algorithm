aad1fe72be7195480a93b6c5b777f5cfcc2fcd4f
Jihyun Han <startingfrom33@gmail.com>
2024-02-10 02:56:24 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,24 +1,27 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-	public static void main(String args[]){
-		Scanner sc = new Scanner(System.in);
-		int Num[] = new int[9];
+public class Main{
+    public static void main(String[] args){
+        
+        try(BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in));)
+        {
+            int max = 0;
+            int idx = 0;
+        
+            for(int i=0; i<9; i++){
+                int current = Integer.parseInt(br.readLine());
+                if(max < current){
+                    idx = i+1;
+                    max = current;
+                }
+            }
 
-		int Max = 0;
-		int MaxCount = 0;
-
-		for (int i = 0; i < Num.length; i++) {
-			Num[i] = sc.nextInt();
-		}
-		//최댓값 구하기
-		for (int k = 0; k < Num.length; k++) {
-			if (Num[k] > Max) {
-				Max = Num[k];
-				MaxCount = k + 1;
-			}
-		}
-		System.out.println(Max);
-		System.out.println(MaxCount);
-	}
-}
+            System.out.println(max);
+            System.out.println(idx);    
+        }
+        catch(Exception e){e.printStackTrace();}
+        
+    }//main ends
+}//class ends
