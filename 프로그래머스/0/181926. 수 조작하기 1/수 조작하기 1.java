ac5ec29188ab186ea5def6161a3f272bf8910a23
Jihyun Han <startingfrom33@gmail.com>
2024-02-09 23:42:38 +0900

    [level 0] Title: 수 조작하기 1, Time: 22.92 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -2,24 +2,13 @@ class Solution {
     public int solution(int n, String control) {
         String[] arr = control.split("");
         
-        for(String a:arr){
-            if(a.equals("w")){
-                n += 1;
-                continue;
+        for(String a:arr)
+            switch(a){
+                case "w": n+=1; break;
+                case "s": n-=1; break;
+                case "d": n+=10; break;
+                case "a": n-=10; break;
             }
-            else if(a.equals("s")){
-                n -= 1;
-                continue;
-            }
-            else if(a.equals("d")){
-                n += 10;
-                continue;
-            }
-            else if(a.equals("a")){
-                n -= 10;
-                continue;
-            }
-        }
         
         return n;
     }
