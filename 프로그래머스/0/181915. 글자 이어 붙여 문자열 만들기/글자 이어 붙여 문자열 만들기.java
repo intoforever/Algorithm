e05fe0f94d012b0e2b11fe87e3b978f3d743d7a9
Jihyun Han <startingfrom33@gmail.com>
2024-02-11 22:23:02 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
@@ -2,11 +2,10 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string, int[] index_list) {
-        String[] s = my_string.split("");
         StringBuilder sb = new StringBuilder();
         
         for(int i:index_list)
-            sb.append(s[i]);
+            sb.append(my_string.charAt(i));
         
         return sb.toString();
     }
