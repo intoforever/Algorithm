075058bb504f1f7585b712fdc5ebca719ae49bd5
Jihyun Han <startingfrom33@gmail.com>
2024-02-13 15:36:49 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.10 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -2,21 +2,10 @@ import java.util.*;
 
 class Solution {
     public int solution(String myString, String pat) {
-        int answer = 0;
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i=0; i<myString.length(); i++){
-            String temp = String.valueOf(myString.charAt(i));
-            if(temp.equals("A"))
-                sb.append("B");
-            else
-                sb.append("A");
-        }
-        
-        if(sb.toString().contains(pat))
-            answer = 1;
-        
-        
-        return answer;
+        StringBuffer sb = new StringBuffer();
+        for (char ch : myString.toCharArray())
+            if (ch == 'A') sb.append('B');
+            else sb.append('A');
+        return sb.toString().contains(pat) ? 1 : 0;
     }
 }
