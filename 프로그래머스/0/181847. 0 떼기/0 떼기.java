843b4fd5bd064dcf9bae97a768d5c0ff83428df4
Jihyun Han <startingfrom33@gmail.com>
2024-02-13 18:10:42 +0900

    [level 0] Title: 0 떼기, Time: 8.44 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,20 +1,6 @@
-import java.util.*;
-
 class Solution {
     public String solution(String n_str) {
-        StringBuilder sb = new StringBuilder();
-        boolean isFirstNotZero = false;
         
-        for(String n:n_str.split("")){
-            if(!n.equals("0")){
-                isFirstNotZero = true;
-            }
-            
-            if(isFirstNotZero)
-                sb.append(n);
-        }
-        
-        String answer = sb.toString();
-        return answer;
+        return ""+Integer.parseInt(n_str);
     }
 }
