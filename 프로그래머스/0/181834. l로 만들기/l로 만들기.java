9a5d3b244c4e009d1bb93f2596ac04aca93c3218
Jihyun Han <startingfrom33@gmail.com>
2024-02-16 00:18:30 +0900

    [level 0] Title: l로 만들기, Time: 21.25 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,20 +1,7 @@
-import java.util.*;
-
 class Solution {
     public String solution(String myString) {
-        StringBuilder sb = new StringBuilder();
+        String answer = myString.replaceAll("[^l-z]", "l");
         
-        for(int i=0; i<myString.length(); i++){
-            char c = myString.charAt(i);
-            
-            if(c<'l')
-                sb.append(String.valueOf('l'));
-            else
-                sb.append(String.valueOf(c));
-            
-        }
-        
-        String answer = sb.toString();
         return answer;
     }
 }
