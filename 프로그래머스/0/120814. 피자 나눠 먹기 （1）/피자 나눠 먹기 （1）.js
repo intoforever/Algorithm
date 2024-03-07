a4ec3df2b13a73132e949570b5fe17a0724a6c22
Jihyun Han <startingfrom33@gmail.com>
2024-03-07 23:58:29 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,13 +1,5 @@
 function solution(n) {
-    var answer = 0;
-    
-    if(Math.trunc(n/7) == 0){
-        answer = 1;
-    }else if(Math.trunc(n%7) == 0){
-        answer = Math.trunc(n/7);
-    }else if(Math.trunc(n%7) < 7){
-        answer = Math.trunc(n/7) + 1;
-    }
+    var answer = Math.ceil(n/7);
     
     return answer;
 }
