e1f8a3b122281f53b5e99495ba29968598c09f31
Jihyun Han <startingfrom33@gmail.com>
2024-03-07 23:24:06 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -2,7 +2,7 @@ function solution(n, k) {
     var answer = 0;
     var freeDrink = Math.trunc(n/10);
     
-    answer = n*12000 + k*2000 - freeDrink*2000;
+    answer = n*12000 + (k - freeDrink)*2000;
     
     return answer;
 }
