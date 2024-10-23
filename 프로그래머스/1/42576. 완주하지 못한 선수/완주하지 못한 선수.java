99b12c3482f3c419add7de50f33b71f7aed57551
Jihyun Han <startingfrom33@gmail.com>
2024-10-23 23:47:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 95.58 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,24 +3,15 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        Map<String, Integer> map = new HashMap<>();
-        
-        for (String p : participant) {
-            map.merge(p, 1, Integer::sum);
-        }
-        
-        for (String c : completion) {
-            map.compute(c, (k, v) -> c.equals(k) ? v - 1 : v);
-        }
-        
-        Set<String> keys = map.keySet();
-        
-        for (String k : keys) {
-            if (map.get(k) == 0) {
-                continue;
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
+
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
             }
-            answer = k;
-        } 
+        }
         return answer;
     }
 }
