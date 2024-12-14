f2ab1629f0eed1268a65dec3950208d97fdd880a
Jihyun Han <startingfrom33@gmail.com>
2024-12-14 21:42:48 +0900

    [level 2] Title: 전화번호 목록, Time: 297.05 ms, Memory: 226 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book, (p1, p2) -> p1.length() - p2.length());
+        
         HashMap<String,Integer> map = new HashMap<>();
         for (String num : phone_book){
             map.put(num, 0);
@@ -9,7 +11,7 @@ class Solution {
 
         for (String phone : phone_book){
             for (int i = 0 ; i < phone.length() ; i++){
-                if(map.containsKey(phone.substring(0, i))){
+                if(map.containsKey(phone.substring(0, i))){ // 긴거 기준으로 생각
                     return false;
                 }
             }
