81d535de586514b4388be7a05dbb799cc4746c21
Jihyun Han <startingfrom33@gmail.com>
2024-12-22 22:17:46 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.08 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        double result = Math.sqrt((double) n);
+        
+        if (isInteger(result)) {
+            result = result + 1;
+            answer = (long) Math.pow(result, 2);
+        } else {
+            answer = -1;
+        }
+        
+        return answer;
+    } // main ends
+    
+    private boolean isInteger (double num) {
+        return num == (int) num;
+    }
+} // class ends
