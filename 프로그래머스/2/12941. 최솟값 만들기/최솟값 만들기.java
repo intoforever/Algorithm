84a96e511064aa7f81070897604ee4da8775cf91
Jihyun Han <startingfrom33@gmail.com>
2024-12-23 21:30:53 +0900

    [level 2] Title: 최솟값 만들기, Time: 3.42 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -7,25 +7,12 @@ class Solution
         
         Arrays.sort(A);
         Arrays.sort(B);
-        reverseArray(B);
         
         for (int i = 0; i < A.length; i++) {
-            answer += A[i] * B[i];
+            answer += A[i] * B[B.length - 1 - i];
         }
 
         return answer;
     } // solution ends
-    
-    private void reverseArray(int[] arr) {
-        int left = 0;
-        int right = arr.length - 1;
-        while(left < right){
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;
-            right--;
-        }
-    }
     
 } // class ends
