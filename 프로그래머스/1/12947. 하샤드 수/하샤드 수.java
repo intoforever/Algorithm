7a62adb6c040cd7fb1866cc1b41520b1329c6f42
Jihyun Han <startingfrom33@gmail.com>
2025-01-14 00:01:53 +0900

    [level 1] Title: 하샤드 수, Time: 3.24 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,15 +1,7 @@
 class Solution {
     public boolean solution(int x) {
-        String[] strArr = String.valueOf(x).split("");
-        int len = strArr.length;
-        int[] intArr = new int[len];
-        int sum = 0;
+        int sum = String.valueOf(x).chars().map(ch -> ch - '0').sum();
         
-        for (int i = 0; i < len; i ++) {
-            intArr[i] = Integer.parseInt(strArr[i]);
-            sum += intArr[i];
-        }
-        
-        return (x % (sum) == 0 ? true : false);
+        return x % sum == 0;
     }
 }
