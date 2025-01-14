7663e2f7e20b98b6d02f83b96874800d72530cd4
Jihyun Han <startingfrom33@gmail.com>
2025-01-15 01:24:36 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.58 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -2,33 +2,17 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String my_string) {
-        String[] array = my_string.split("");
-        List<Integer> list = new ArrayList<>();
-        
-        for (String a : array) {
-            if (!checkIsNumber(a)) {
-                continue;
-            }
-            
-            list.add(Integer.parseInt(a));
+
+        my_string = my_string.replaceAll("[a-z]","");
+
+        int[] answer = new int[my_string.length()];
+
+        for(int i =0; i<my_string.length(); i++){
+            answer[i] = my_string.charAt(i) - '0';
         }
-        
-        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
+
         Arrays.sort(answer);
-        
+
         return answer;
-    }
-    
-    private boolean checkIsNumber(String s) {
-        boolean isNumber = false;
-        
-        try {
-            Integer.parseInt(s);
-            isNumber = true;
-        } catch (Exception e) {
-            isNumber = false;
-        }
-        
-        return isNumber;
     }
 }
