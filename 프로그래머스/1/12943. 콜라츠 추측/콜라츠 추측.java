47fe9695aac9b8ed56b2a8b04b24c002344e7767
Jihyun Han <startingfrom33@gmail.com>
2025-01-18 19:37:19 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -3,12 +3,26 @@ class Solution {
         int answer = 0;
         long num2 = num;
         
-        while (num2 != 1 && answer < 500) {
+//         while (num2 != 1 && answer < 500) {
+//             if (num2 % 2 == 0) {
+//                 num2 /= 2;
+//             } else {
+//                 num2 = num2 * 3 + 1;
+//             }
+//             answer++;
+//         }
+        
+        for (int i = 0; i < 500; i++) {
+            if (num2 == 1) {
+                break;
+            }
+            
             if (num2 % 2 == 0) {
                 num2 /= 2;
             } else {
                 num2 = num2 * 3 + 1;
             }
+            
             answer++;
         }
             
