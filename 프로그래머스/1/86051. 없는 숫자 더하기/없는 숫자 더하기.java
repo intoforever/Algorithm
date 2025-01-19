28240439a41f6049c37be9e8e93a34062e011766
Jihyun Han <startingfrom33@gmail.com>
2025-01-19 21:58:59 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,18 +1,9 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        boolean[] flag = new boolean[10];
-
-        for (int n : numbers) {
-            flag[n] = true;
+        int sum = 45;
+        for (int i : numbers) {
+            sum -= i;
         }
-
-        for (int i = 0; i < flag.length; i++) {
-            if (!flag[i]) answer += i;
-        }
-
-        return answer;
+        return sum;
     }
 }
