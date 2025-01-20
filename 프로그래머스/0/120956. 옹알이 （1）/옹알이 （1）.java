5d6e70d14677f6fc4661112f650d284a2bdb594c
Jihyun Han <startingfrom33@gmail.com>
2025-01-20 22:31:43 +0900

    [level 0] Title: 옹알이 (1), Time: 1.36 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,31 +1,18 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        
-        for (int i = 0; i < babbling.length; i++) {
-            String bab = babbling[i];
-            bab = checkCanBubble(bab);
-            
-            if (bab.length() == 0) {
-                answer++;    
+
+        for(int i =0; i < babbling.length; i++) {
+            babbling[i] = babbling[i].replace("aya", "1");
+            babbling[i] = babbling[i].replace("woo", "1");
+            babbling[i] = babbling[i].replace("ye", "1");
+            babbling[i] = babbling[i].replace("ma", "1");
+            babbling[i] = babbling[i].replace("1", "");
+            if(babbling[i].isEmpty()) {
+                answer = answer + 1;
             }
         }
-        
+
         return answer;
-    }
-    
-    private String checkCanBubble(String word) {
-        String[] canBabbleStr = {"aya", "woo", "ma", "ye"};
-        int idx = -1;
-        
-        if (word.startsWith("aya") || word.startsWith("woo")) {
-            idx = 3;
-        } else if (word.startsWith("ye") || word.startsWith("ma")) {
-            idx = 2;
-        } else {
-            return word;
-        }
-        
-        return checkCanBubble(word.substring(idx));
     }
 }
