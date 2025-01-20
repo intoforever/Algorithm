de77b345c13e11d6f48ee63439b8bad3200f53af
Jihyun Han <startingfrom33@gmail.com>
2025-01-20 22:04:53 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 3.16 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,21 +1,12 @@
 class Solution {
-    public String solution(String phone_number) {
-        int len = phone_number.length();
-        
-        if (len <= 4) {
-            return phone_number;
-        }
-            
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < len; i++) {
-            if (i <= len - 5) {
-                sb.append("*");
-            } else {
-                sb.append(phone_number.charAt(i));
-            }
-        }
-        
-        return sb.toString();
-    }
+  public String solution(String phone_number) {
+      String answer = "";
+      int size = phone_number.length();
+
+      for (int i = 0; i < size - 4; i++) {
+          answer += "*";
+      }
+
+      return answer += phone_number.substring(size - 4, size);
+  }
 }
