a517593c3b7536ea004be0d3006a9c6b7e63e2b2
Jihyun Han <startingfrom33@gmail.com>
2025-01-21 22:07:32 +0900

    [level 0] Title: 배열 회전시키기, Time: 2.67 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -1,35 +1,22 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers, String direction) {
-        int len = numbers.length;
-        int[] answer = new int[len];
-        int targetIdx = -1;
+        Deque<Integer> q = new LinkedList<>();
+        int target = 0;
         
-        switch (direction) {
-            // right면 끝에거 먼저 담고 0, 1, 2, ... len-1을 맨 처음
-            case "right":
-                targetIdx = len - 1;
-                
-                answer[0] = numbers[targetIdx];
-                for (int i = 0; i < len - 1; i++) {
-                    answer[i + 1] = numbers[i];
-                }
-                
-                break;
-                
-            // left면 1번부터 먼저 다 담고 0번을 맨 마지막 0을 맨 마지막
-            case "left":
-                targetIdx = 0;
-                
-                for (int i = 0; i < len - 1; i++) {
-                    answer[i] = numbers[i + 1];
-                }
-                answer[len - 1] = numbers[targetIdx];
-                
-                break;
-            default:
-                break;
+        for (int n : numbers) {
+            q.add(n);
         }
         
-        return answer;
+        if ("right".equals(direction)) {
+            target = q.pollLast();
+            q.offerFirst(target);
+        } else {
+            target = q.pollFirst();
+            q.offerLast(target);
+        }
+        
+        return q.stream().mapToInt(Integer::intValue).toArray();
     }
 }
