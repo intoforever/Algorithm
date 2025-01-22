ed73783fb11365e944fce35aca7c1c1c6a5cf349
Jihyun Han <startingfrom33@gmail.com>
2025-01-23 00:17:43 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.55 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -1,16 +1,8 @@
 import java.util.*;
-
 class Solution {
     public String solution(String my_string) {
-        StringBuilder sb = new StringBuilder();
-        
-        char[] arr = my_string.toLowerCase().toCharArray();
-        Arrays.sort(arr);
-        
-        for (char a : arr) {
-            sb.append(a);
-        }
-        
-        return sb.toString();
+        char[] c = my_string.toLowerCase().toCharArray();
+        Arrays.sort(c);
+        return new String(c);
     }
 }
