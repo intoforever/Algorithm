7619eaa8ae3cb90a7f8f73a79f8b6834981ca9c0
Jihyun Han <startingfrom33@gmail.com>
2025-01-22 19:02:51 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 4.13 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(int left, int right) {
         int answer = 0;
-        int len = right - left + 1;
         
         for (; left <= right; left++) {
             int delimiter = getDelimiter(left); // 1 or -1
