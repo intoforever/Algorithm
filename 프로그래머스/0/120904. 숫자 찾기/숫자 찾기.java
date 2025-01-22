cc954ad66a50752e7900ecd836d16ce5f036dd70
Jihyun Han <startingfrom33@gmail.com>
2025-01-22 18:36:20 +0900

    [level 0] Title: 숫자 찾기, Time: 7.11 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,15 +1,5 @@
 class Solution {
     public int solution(int num, int k) {
-        char[] arr = String.valueOf(num).toCharArray();
-        
-        for (int i = 0; i < arr.length; i++) {
-            int n = arr[i] - '0';
-            
-            if (n == k) {
-                return i + 1;
-            }
-        }
-        
-        return -1;
+        return ("-" + num).indexOf(String.valueOf(k));
     }
 }
