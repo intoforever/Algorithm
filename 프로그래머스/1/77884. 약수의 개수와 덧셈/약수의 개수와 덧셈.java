4d8a548e44bb5887ad00cecdbd7014c37db9e601
Jihyun Han <startingfrom33@gmail.com>
2025-01-22 19:03:16 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.11 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -1,25 +1,18 @@
 class Solution {
     public int solution(int left, int right) {
         int answer = 0;
-        
-        for (; left <= right; left++) {
-            int delimiter = getDelimiter(left); // 1 or -1
-            
-            answer += left * delimiter;
-        }        
-        
-        return answer;
-    }
-    
-    private int getDelimiter(int value) {
-        int cnt = 0;
-        
-        for (int i = 1; i <= value; i++) {
-            if (value % i == 0) {
-                cnt++;
+
+        for (int i=left;i<=right;i++) {
+            //제곱수인 경우 약수의 개수가 홀수
+            if (i % Math.sqrt(i) == 0) {
+                answer -= i;
+            }
+            //제곱수가 아닌 경우 약수의 개수가 짝수
+            else {
+                answer += i;
             }
         }
-        
-        return cnt % 2 == 0 ? 1 : -1;
+
+        return answer;
     }
 }
