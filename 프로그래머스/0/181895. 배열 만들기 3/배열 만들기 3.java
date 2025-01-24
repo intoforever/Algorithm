a030c70aaace29bd470ba8be9c92a78b2cee5892
Jihyun Han <startingfrom33@gmail.com>
2025-01-24 21:20:56 +0900

    [level 0] Title: 배열 만들기 3, Time: 1.78 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -2,15 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
-        List<Integer> list = new ArrayList<>();
+        int len1 = intervals[0][1] - intervals[0][0] + 1;
+        int len2 = intervals[1][1] - intervals[1][0] + 1;
         
-        for (int i = 0; i < intervals.length; i++) {
-            for (int j = intervals[i][0]; j < intervals[i][1] + 1; j++) {
-                list.add(arr[j]);
-            }
+        int[] intervals1 = new int[len1];
+        int[] intervals2 = new int[len2];
+        
+        for (int i = 0, idx = intervals[0][0]; i < len1; i++) {
+            intervals1[i] = arr[idx++];
         }
-
-        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
+        
+        for (int i = 0, idx = intervals[1][0]; i < len2; i++) {
+            intervals2[i] = arr[idx++];
+        }
+        
+        int[] answer = new int[intervals1.length + intervals2.length];
+        System.arraycopy(intervals1, 0, answer, 0, intervals1.length);
+        System.arraycopy(intervals2, 0, answer, intervals1.length, intervals2.length);
         
         return answer;
     }
