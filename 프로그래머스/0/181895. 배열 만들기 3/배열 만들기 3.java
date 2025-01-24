6ee9dba86b49fda5780456e1f5fed6a432c8491e
Jihyun Han <startingfrom33@gmail.com>
2025-01-24 21:31:52 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.21 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -2,23 +2,18 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
-        int len1 = intervals[0][1] - intervals[0][0] + 1;
-        int len2 = intervals[1][1] - intervals[1][0] + 1;
+        int a1 = intervals[0][0];
+        int b1 = intervals[0][1];
+        int a2 = intervals[1][0];
+        int b2 = intervals[1][1];
         
-        int[] intervals1 = new int[len1];
-        int[] intervals2 = new int[len2];
+        int len1 = b1 - a1 + 1;
+        int len2 = b2 - a2 + 1;
         
-        for (int i = 0, idx = intervals[0][0]; i < len1; i++) {
-            intervals1[i] = arr[idx++];
-        }
+        int[] answer = new int[len1 + len2];
         
-        for (int i = 0, idx = intervals[1][0]; i < len2; i++) {
-            intervals2[i] = arr[idx++];
-        }
-        
-        int[] answer = new int[intervals1.length + intervals2.length];
-        System.arraycopy(intervals1, 0, answer, 0, intervals1.length);
-        System.arraycopy(intervals2, 0, answer, intervals1.length, intervals2.length);
+        System.arraycopy(arr, a1, answer, 0, len1);
+        System.arraycopy(arr, a2, answer, len1, len2);
         
         return answer;
     }
