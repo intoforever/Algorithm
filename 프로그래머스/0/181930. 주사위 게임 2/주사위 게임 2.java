79aeebfc7e4dab03e5d625a545b17d1e7744777a
Jihyun Han <startingfrom33@gmail.com>
2025-01-24 21:57:35 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int a, int b, int c) {
         int answer = 1;
-
         int count = 1;
+        
         if(a == b || a == c || b == c) {
             count++;
         }
