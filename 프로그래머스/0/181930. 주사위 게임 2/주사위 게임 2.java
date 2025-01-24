15d714fc2b267a5ae244879d464e6aab56b59a99
Jihyun Han <startingfrom33@gmail.com>
2025-01-24 21:46:44 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.07 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,15 +1,17 @@
 class Solution {
     public int solution(int a, int b, int c) {        
         int sum1 = a + b + c;
-        int sum2 = (int) Math.pow(a, 2) + (int) Math.pow(b, 2) + (int) Math.pow(c, 2);
-        int sum3 = (int) Math.pow(a, 3) + (int) Math.pow(b, 3) + (int) Math.pow(c, 3);
         
         if (a == b && b == c) {
-            return sum1 * sum2 * sum3;
+            return sum1 * pow(a, b, c, 2) * pow(a, b, c, 3);
         } else if (a == b || b == c || a == c) {
-            return sum1 * sum2;
+            return sum1 * pow(a, b, c, 2);
         } else {
             return sum1;
         }
+    }
+    
+    private int pow(int a, int b, int c, int cnt) {
+        return (int) Math.pow(a, cnt) + (int) Math.pow(b, cnt) + (int) Math.pow(c, cnt);
     }
 }
