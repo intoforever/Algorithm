6319ea1b10aa859761d6c51905ec6584086f8a39
Jihyun Han <startingfrom33@gmail.com>
2025-01-26 00:15:26 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 26.51 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,31 +1,109 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 class Solution {
-    public int solution(String[] friends, String[] gifts) {
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < friends.length; i++) {
-            map.put(friends[i], i);
-        }
-        int[] index = new int[friends.length];
-        int[][] record = new int[friends.length][friends.length];
+    public int solution(String[] rawFriends, String[] gifts) {
+        Friends friends = new Friends(rawFriends);
+        friends.sendGifts(gifts);
+        return friends.calculateMaxNextMonth();
+    }
 
-        for (String str : gifts) {
-            String[] cur = str.split(" ");
-            index[map.get(cur[0])]++;
-            index[map.get(cur[1])]--;
-            record[map.get(cur[0])][map.get(cur[1])]++;
+    private static class Friends {
+        private final Map<String, Friend> friends = new HashMap<>();
+
+        public Friends(String[] friends) {
+            for (String friend : friends) {
+                addFriend(friend);
+            }
         }
 
-       int ans = 0;
-       for (int i = 0; i < friends.length; i++) {
-           int cnt = 0;
-           for (int j = 0; j < friends.length; j++) {
-               if(i == j) continue;
-               if (record[i][j] > record[j][i]) cnt++;
-               else if (record[i][j] == record[j][i] && index[i] > index[j]) cnt++; 
-           }
-           ans = Math.max(cnt, ans);
-       }
-        return ans;
+        public void sendGifts(String[] gifts) {
+            for (String gift : gifts) {
+                String[] senderReceiver = gift.split(" ");
+                sendGift(senderReceiver[0], senderReceiver[1]);
+            }
+        }
+
+        public void sendGift(String sender, String receiver) {
+            Friend senderFriend = friends.get(sender);
+            Friend receiverFriend = friends.get(receiver);
+
+            senderFriend.sendGift(receiverFriend);
+            receiverFriend.receiveGift(senderFriend);
+        }
+
+        public int calculateMaxNextMonth() {
+            Map<Friend, Integer> receivedGifts = new HashMap<>();
+
+            for (Friend friend : friends.values()) {
+                for (Friend otherFriend : friends.values()) {
+                    if (friend.isSame(otherFriend)) {
+                        continue;
+                    }
+
+                    if (friend.shouldReceiveFrom(otherFriend)) {
+                        receivedGifts.put(friend, receivedGifts.getOrDefault(friend, 0) + 1);
+                    }
+                }
+            }
+
+            return receivedGifts.values().stream()
+                    .max(Integer::compareTo)
+                    .orElse(0);
+        }
+
+        public void addFriend(String name) {
+            friends.put(name, new Friend(name));
+        }
+    }
+
+    private static class Friend {
+        private final String name;
+        private final Map<Friend, Integer> receivedGifts = new HashMap<>();
+        private final Map<Friend, Integer> sentGifts = new HashMap<>();
+        private int giftIndex = 0;
+
+        public Friend(String name) {
+            this.name = name;
+        }
+
+        public void sendGift(Friend friend) {
+            this.sentGifts.put(friend, sentGifts.getOrDefault(friend, 0) + 1);
+            this.giftIndex++;
+        }
+
+        public void receiveGift(Friend friend) {
+            this.receivedGifts.put(friend, receivedGifts.getOrDefault(friend, 0) + 1);
+            this.giftIndex--;
+        }
+
+        public boolean isSame(Friend friend) {
+            return this.name.equals(friend.name);
+        }
+
+        public boolean shouldReceiveFrom(Friend friend) {
+            int received = receivedGifts.getOrDefault(friend, 0);
+            int sent = sentGifts.getOrDefault(friend, 0);
+
+            if (received == sent) {
+                return this.giftIndex > friend.giftIndex;
+            }
+
+            return received < sent;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Friend friend = (Friend) o;
+            return Objects.equals(name, friend.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
     }
 }
