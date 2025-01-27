e097e63281049b539b7ac271bcbefb85a514444e
Jihyun Han <startingfrom33@gmail.com>
2025-01-27 21:15:25 +0900

    [level 0] Title: 글자 지우기, Time: 0.58 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -10,12 +10,7 @@ class Solution {
             str[target] = "";
         }
         
-        StringBuilder sb = new StringBuilder();
         
-        for (int i = 0; i < str.length; i++) {
-            sb.append(str[i]);
-        }
-        
-        return sb.toString();
+        return String.join("", str);
     }
 }
