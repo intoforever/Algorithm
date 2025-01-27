71c77d3eef6fc18eeb98a87023c70f2ff4ec3215
Jihyun Han <startingfrom33@gmail.com>
2025-01-27 21:01:27 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.64 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/README.md
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수열과 구간 쿼리 1 - 181883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181883#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181883) 
 
 ### 성능 요약
 
-메모리: 86.1 MB, 시간: 1.70 ms
+메모리: 79.2 MB, 시간: 2.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 27일 21:00:34
+2025년 01월 27일 21:01:23
 
 ### 문제 설명
 
--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -1,15 +1,11 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        for (int i = 0; i < queries.length; i++) {
-            int[] query = queries[i];
-            int s = query[0];
-            int e = query[1];
 
-            for (int j = s; j <= e; j++) {
-                arr[j]++;
-            }
+        for (int[] query : queries) {
+            int s = query[0], e = query[1];
+            for (int i = s; i <= e; i++)
+                arr[i]++;
         }
-        
         return arr;
     }
 }
