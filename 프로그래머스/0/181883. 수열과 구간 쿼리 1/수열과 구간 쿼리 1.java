e6042641ac5e0cdfa8fa94780f7350dc9ea1b46d
Jihyun Han <startingfrom33@gmail.com>
2025-01-27 21:00:37 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 1.70 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -1,32 +1,15 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < arr.length; i++) {
-            map.put(i, arr[i]);
-        }
-        
         for (int i = 0; i < queries.length; i++) {
-            processQuery(map, queries[i]);
+            int[] query = queries[i];
+            int s = query[0];
+            int e = query[1];
+
+            for (int j = s; j <= e; j++) {
+                arr[j]++;
+            }
         }
         
-        int[] answer = new int[arr.length];
-        
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = map.get(i);
-        }
-        
-        return answer;
-    }
-    
-    private void processQuery(Map<Integer, Integer> map, int[] query) {
-        int s = query[0];
-        int e = query[1];
-        
-        for (int i = s; i <= e; i++) {
-            map.put(i, map.get(i) + 1);
-        }
+        return arr;
     }
 }
