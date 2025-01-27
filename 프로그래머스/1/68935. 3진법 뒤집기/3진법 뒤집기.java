35ce574bd1ca33d1040c0b35de1e43c93cedf0d0
Jihyun Han <startingfrom33@gmail.com>
2025-01-28 01:52:09 +0900

    [level 1] Title: 3진법 뒤집기, Time: 10.36 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,24 +1,14 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        int m = n;
-        int power = 1;
-        
-        while (m > 0) {
-            m /= 3;
-            
-            if (m > 0) {
-                power *= 3;
-            }
-        }
-        
-        while (n > 0) {
-            int remainder = n % 3;
-            answer += remainder * power;
-            power /= 3;
+        String a = "";
+
+        while(n > 0){
+            a = (n % 3) + a;
             n /= 3;
         }
-        
-        return answer;
+        a = new StringBuilder(a).reverse().toString();
+
+
+        return Integer.parseInt(a,3);
     }
 }
