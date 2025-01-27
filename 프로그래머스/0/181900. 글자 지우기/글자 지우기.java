05a8507c80cb6f0be0ed38136466bc866f459004
Jihyun Han <startingfrom33@gmail.com>
2025-01-27 21:12:52 +0900

    [level 0] Title: 글자 지우기, Time: 0.44 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     public String solution(String my_string, int[] indices) {
         String[] str = my_string.split("");
-        Arrays.sort(indices);
         
         for (int i = 0; i < indices.length; i++) {
             int target = indices[i];
