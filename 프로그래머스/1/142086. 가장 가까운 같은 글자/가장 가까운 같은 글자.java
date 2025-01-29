5193abe2b0d349e87a20119e58bb9cf5f5e8c5e4
Jihyun Han <startingfrom33@gmail.com>
2025-01-30 04:43:07 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.41 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -3,13 +3,14 @@ import java.util.*;
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
+        char[] arr = s.toCharArray();
         HashMap<Character,Integer> map = new HashMap<>();
         
-        for(int i=0; i<s.length();i++){
-            char ch = s.charAt(i);
-            answer[i] = i-map.getOrDefault(ch,i+1);
-            map.put(ch,i);
+        for(int i = 0; i < arr.length; i++){
+            answer[i] = i - map.getOrDefault(arr[i], i + 1);
+            map.put(arr[i], i);
         }
+        
         return answer;
     }
 }
