20ae677eb5744b4fe8992bf41aabaf2ce30a0a61
Jihyun Han <startingfrom33@gmail.com>
2025-01-30 03:25:34 +0900

    [level 1] Title: 시저 암호, Time: 0.81 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,11 +1,10 @@
 class Solution {
     public String solution(String s, int n) {
         StringBuilder sb = new StringBuilder();
-        char[] arr = s.toCharArray();
         
-        for (char a : arr) {
+        for (char a : s.toCharArray()) {
             if (a != ' ') {
-                if ((97 <= a && a <= 122 - n) || (65 <= a && a <= 90 - n)) {
+                if (('a' <= a && a <= 'z' - n) || ('A' <= a && a <= 'Z' - n)) {
                     a = (char) (a + n);
                 } else {
                     a = (char) (a + n - 26);
