c8eb39e612426f744ebdd4a7e0dcf4bab1ec2158
Jihyun Han <startingfrom33@gmail.com>
2025-02-02 21:27:10 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.60 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -1,127 +1,29 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int a, int b, int c, int d) {
-        int answer = 0;
-        
-        DiceGame diceGame = new DiceGame(a, b, c, d);
-        int cases = diceGame.getCase();
-        
-        if (cases == 1) {
-            answer = diceGame.processFirstCase();
-        } else if (cases == 2) {
-            answer = diceGame.processSecondCase();
-        } else if (cases == 3) {
-            answer = diceGame.processThirdCase();
-        } else if (cases == 4) {
-            answer = diceGame.processFourthCase();
-        } else if (cases == 5) {
-            answer = diceGame.processFifthCase();
-        }
-        
-        return answer;
-    }
-}
 
-class DiceGame {
-    int a;
-    int b;
-    int c;
-    int d;
-    
-    int[] keys;
-    Map<Integer, Integer> map;
-    int size;
-    
-    public DiceGame(int a, int b, int c, int d) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-        this.d = d;
-        
-        this.keys = new int[4];
-        keys[0] = a;
-        keys[1] = b;
-        keys[2] = c;
-        keys[3] = d;
-        
-        this.map = new HashMap<>();
-        for (int key : keys) {
-            map.put(key, map.getOrDefault(key, 0) + 1);
+        int[] dice = { a, b, c, d };
+        Arrays.sort(dice);
+
+        int ans = 0;
+
+        if (dice[0] == dice[3]) {
+            ans = 1111 * dice[3];
+        } else if (dice[0] == dice[2] || dice[1] == dice[3]) {
+            ans = (int) Math.pow(dice[1] * 10 + (dice[0] + dice[3] - dice[1]), 2);
+        } else if (dice[0] == dice[1] && dice[2] == dice[3]) {
+            ans = (dice[0] + dice[3]) * (dice[3] - dice[0]);
+        } else if (dice[0] == dice[1]) {
+            ans = dice[2] * dice[3];
+        } else if (dice[1] == dice[2]) {
+            ans = dice[0] * dice[3];
+        } else if (dice[2] == dice[3]) {
+            ans = dice[0] * dice[1];
+        } else {
+            ans = dice[0];
         }
-        
-        this.size = map.keySet().size();
-    }
-    
-    public int getCase() {
-        int cases = 0;
-        
-        if (this.size == 1) {
-            cases = 1;
-        } else if (this.size == 2) {
-            if (this.map.get(this.a) != 2) {
-                cases = 2;
-            } else {
-                cases = 3;
-            }
-        } else if (this.size == 3) {
-            cases = 4;
-        } else if (this.size == 4) {
-            cases = 5;
-        }
-        
-        return cases;
-    }
-    
-    public int processFirstCase() {
-        return 1111 * this.a;
-    }
-    
-    public int processSecondCase() {
-        int p = 0;
-        int q = 0;
-        
-        for (int key : map.keySet()) {
-            if (map.get(key) == 3) {
-                p = key;
-            } else {
-                q = key;
-            }
-        }
-        
-        return (int) Math.pow((10 * p + q), 2);
-    }
-    
-    public int processThirdCase() {
-        int p = 0;
-        int q = 0;
-        List<Integer> list = new ArrayList<>(map.keySet());
-        
-        p = list.get(0);
-        q = list.get(1);
-        
-        return (p + q) * Math.abs(p - q);
-    }
-    
-    public int processFourthCase() {
-        int p = 0;
-        int q = 0;
-        int r = 0;
-        
-        for (int key : map.keySet()) {
-            if (map.get(key) == 2) {
-                p = key;
-            } else if (q == 0) {
-                q = key;
-            } else {
-                r = key;
-            }
-        }
-        
-        return q * r;
-    }
-    
-    public int processFifthCase() {
-        return Math.min(a, Math.min(b, Math.min(c, d)));
+
+        return ans;
     }
 }
