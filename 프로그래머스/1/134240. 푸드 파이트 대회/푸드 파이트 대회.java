3c824894637d97a333d007fa49239ed3ddc4a64a
Jihyun Han <startingfrom33@gmail.com>
2025-02-07 00:13:45 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 17.31 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -3,18 +3,9 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         
         for (int i = 1; i < food.length; i++) {
-            int foodNum = food[i];
-            
-            for (int j = 0; j < foodNum / 2; j++) {
-                sb.append(i);
-            }
+            sb.append(Integer.toString(i).repeat(food[i] / 2));
         }
         
-        StringBuilder sb2 = new StringBuilder();
-        sb2.append(sb.toString());
-        sb2.append(0);
-        sb2.append(sb.reverse().toString());
-        
-        return sb2.toString();
+        return sb.toString() + "0" + sb.reverse().toString();
     }
 }
