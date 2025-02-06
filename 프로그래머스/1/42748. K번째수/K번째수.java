2a1a68af47ad75efa97e8158ef1563d19d8899e1
Jihyun Han <startingfrom33@gmail.com>
2025-02-07 00:27:19 +0900

    [level 1] Title: K번째수, Time: 0.51 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,16 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int len = commands.length;
-        int[] answer = new int[len];
+        int[] answer = new int[commands.length];
         
-        for (int x = 0; x < len; x++) {
+        for (int x = 0; x < commands.length; x++) {
             int i = commands[x][0];
             int j = commands[x][1];
             int k = commands[x][2];
+            int len = j - i + 1;
             
-            int[] newArr = new int[j - i + 1];
-            System.arraycopy(array, i - 1, newArr, 0, j - i + 1);
+            int[] newArr = new int[len];
+            System.arraycopy(array, i - 1, newArr, 0, len);
             Arrays.sort(newArr);
             
             answer[x] = newArr[k - 1];
