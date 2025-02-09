53d8ff5ac1b6a9866c5528f0a899fcb61427d629
Jihyun Han <startingfrom33@gmail.com>
2025-02-10 00:30:11 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,17 +1,5 @@
 class Solution {
     public int solution(int a, int b, int n) {
-        int answer = 0;
-        int quotient = 0;
-        int newCoke = 0;
-        
-        while (n >= a) {
-            quotient = a * (n / a);
-            newCoke = (n / a) * b;
-            answer += newCoke;
-            
-            n = (n - quotient + newCoke);
-        }
-        
-        return answer;
+        return (n - b) / (a - b) * b;
     }
 }
