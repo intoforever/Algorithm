4fb74fe277e0d9af38d0c0f900d54e8f175ba937
Jihyun Han <startingfrom33@gmail.com>
2025-02-10 01:52:55 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.86 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -2,38 +2,60 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int k, int[] score) {
-        int[] answer = new int[score.length];
-        int[] temp = new int[k];
-        int min = score[0];
-        temp[0] = score[0];
-        answer[0] = score[0];
+        HonorClass honorClass = new HonorClass(k, score);
         
-        for (int i = 1; i < score.length; i++) {
-            int num = score[i];
+        return honorClass.checkAnnounceScore();
+    }
+}
+
+class HonorClass {
+    private int[] answer;
+    private int[] temp;
+    private int[] score;
+    private int min;
+    private int k;
+    
+    public HonorClass(int k, int[] score) {
+        this.answer = new int[score.length];
+        this.temp = new int[k];
+        this.score = score;
+        this.k = k;
+        
+        int num = score[0];
+        
+        this.answer[0] = num;
+        this.temp[0] = num;
+        this.min = num;
+    }
+    
+    public int[] checkAnnounceScore() {
+        for (int i = 1; i < this.score.length; i++) {
+            int num = this.score[i];
             
-            if (i < k) {
-                temp[i] = num;
+            if (i < this.k) {
+                this.temp[i] = num;
                 
-                if (min > num) {
-                    min = num;
+                if (this.min > num) {
+                    this.min = num;
                 }
                 
-                answer[i] = min;
+                this.answer[i] = min;
                 
-                if (i == k - 1) {
-                    Arrays.sort(temp);
+                if (i == this.k - 1) {
+                    Arrays.sort(this.temp);
                 }
                 
                 continue;
             }
             
-            if (temp[0] < num) {
-                temp[0] = num;
-                Arrays.sort(temp);
-                min = temp[0];
+            if (this.temp[0] < num) {
+                this.temp[0] = num;
+                Arrays.sort(this.temp);
+                
+                this.min = this.temp[0];
             }
             
-            answer[i] = min;
+            this.answer[i] = this.min;
         }
         
         return answer;
