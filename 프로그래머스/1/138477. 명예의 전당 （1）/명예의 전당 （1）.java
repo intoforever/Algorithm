35df7b7188819a5bd5d5615c8d3964eb2e61734b
Jihyun Han <startingfrom33@gmail.com>
2025-02-10 01:56:59 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.25 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -2,62 +2,20 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int k, int[] score) {
-        HonorClass honorClass = new HonorClass(k, score);
-        
-        return honorClass.checkAnnounceScore();
-    }
-}
+        int[] answer = new int[score.length];
+        Queue<Integer> priorityQueue = new PriorityQueue<>();
+        int temp = 0;
 
-class HonorClass {
-    private int[] answer;
-    private int[] temp;
-    private int[] score;
-    private int min;
-    private int k;
-    
-    public HonorClass(int k, int[] score) {
-        this.answer = new int[score.length];
-        this.temp = new int[k];
-        this.score = score;
-        this.k = k;
-        
-        int num = score[0];
-        
-        this.answer[0] = num;
-        this.temp[0] = num;
-        this.min = num;
-    }
-    
-    public int[] checkAnnounceScore() {
-        for (int i = 1; i < this.score.length; i++) {
-            int num = this.score[i];
+        for(int i = 0; i < score.length; i++) {
+            priorityQueue.add(score[i]);
             
-            if (i < this.k) {
-                this.temp[i] = num;
-                
-                if (this.min > num) {
-                    this.min = num;
-                }
-                
-                this.answer[i] = min;
-                
-                if (i == this.k - 1) {
-                    Arrays.sort(this.temp);
-                }
-                
-                continue;
+            if (priorityQueue.size() > k) {
+                priorityQueue.poll();
             }
-            
-            if (this.temp[0] < num) {
-                this.temp[0] = num;
-                Arrays.sort(this.temp);
-                
-                this.min = this.temp[0];
-            }
-            
-            this.answer[i] = this.min;
+
+            answer[i] = priorityQueue.peek();
         }
-        
+
         return answer;
     }
 }
