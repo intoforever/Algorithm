3684c47c6e9545a6f778a5eccc50df816f99bc89
Jihyun Han <startingfrom33@gmail.com>
2025-02-09 23:22:02 +0900

    [level 0] Title: 배열 만들기 5, Time: 5.61 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -2,28 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] intStrs, int k, int s, int l) {
-        int cnt = 0;
+        List<Integer> list = new ArrayList<>();
         
         for (int i = 0; i < intStrs.length; i++) {
             int num = Integer.parseInt(intStrs[i].substring(s, s + l));
             
             if (k < num) {
-                cnt++;
+                list.add(num);
             }
             
         }
         
-        int[] answer = new int[cnt];
-        
-        for (int i = 0, idx = 0; i < intStrs.length; i++) {
-            int num = Integer.parseInt(intStrs[i].substring(s, s + l));
-            
-            if (k < num) {
-                answer[idx++] = num;
-            }
-            
+        int[] answer = new int[list.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
         }
-        
+
         return answer;
     }
 }
