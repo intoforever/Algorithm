aa78617e9d73afd1bdd1e2e6e3fb7ff7b04d6b39
Jihyun Han <startingfrom33@gmail.com>
2025-02-10 22:33:34 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.11 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -1,10 +1,9 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        for (int k = 0; k < queries.length; k++) {
-            int i = queries[k][0];
-            int j = queries[k][1];
-            int temp = arr[i];
+        for (int[] q : queries) {
+            int i = q[0], j = q[1];
             
+            int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
         }
