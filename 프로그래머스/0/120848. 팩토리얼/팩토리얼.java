484cbfbcbbe934a843d45ce3366cdd62fe747426
Jihyun Han <startingfrom33@gmail.com>
2025-02-16 00:08:37 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -1,19 +1,13 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        int num = 1;
-        int val = 1;
-        
-        
-        while (true) {
-            if (val > n) {
-                break;
-            }
-            
-            answer = num++;
-            val *= num;
+        int answer = 1;
+        int factorial = 1;
+
+        while(n >= factorial) {
+            answer ++;
+            factorial *= answer;
         }
         
-        return answer;
+        return answer - 1 ;
     }
 }
