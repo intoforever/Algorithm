903501f6a44941b0517ad79f783f5267db691650
Jihyun Han <startingfrom33@gmail.com>
2025-02-16 20:08:42 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 13.74 ms, Memory: 91.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String[] arr = s.split("");
+        Map<String, Integer> map = new LinkedHashMap<>();
+        StringBuilder sb = new StringBuilder();
+        
+        Arrays.sort(arr);
+        
+        for (String a : arr) {
+            map.put(a, map.getOrDefault(a, 0) + 1);
+        }
+        
+        for (String key : map.keySet()) {
+            System.out.println(key + " " + map.get(key));
+            
+            if (map.get(key) > 1) {
+                continue;
+            }
+            
+            sb.append(key);
+        }
+        
+        return sb.toString();
+    }
+}
