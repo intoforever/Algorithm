23a488e4f2d65e0cc4b86b996da57bddaf19169a
Jihyun Han <startingfrom33@gmail.com>
2025-02-17 21:51:52 +0900

    [level 0] Title: 세 개의 구분자, Time: 96.01 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,11 +1,12 @@
 class Solution {
     public String[] solution(String myStr) {
         myStr = myStr.replaceAll("[abc]", " ").trim();
+        String[] answer = {"EMPTY"};
         
-        if (myStr.length() == 0) {
-            return new String[]{"EMPTY"};
+        if (myStr.length() != 0) {
+            answer = myStr.split("\\s+");
         }
         
-        return myStr.split("\\s+");
+        return answer;
     }
 }
