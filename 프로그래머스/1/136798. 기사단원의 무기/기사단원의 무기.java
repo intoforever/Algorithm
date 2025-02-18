d7a3bc02129db13323bd2ed322cbd711ed623fb5
Jihyun Han <startingfrom33@gmail.com>
2025-02-18 21:30:50 +0900

    [level 1] Title: 기사단원의 무기, Time: 9.56 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,29 +1,19 @@
 class Solution {
     public int solution(int number, int limit, int power) {
-        int answer = 0;
-        
+       int[] count = new int[number + 1];    
         for (int i = 1; i <= number; i++) {
-            int cnt = countDividers(i);
-            answer += cnt > limit ? power : cnt;
-        }
-        
-        return answer;
-    }
-    
-    private int countDividers(int n) {
-        int cnt = 0;
-        int sqrtN = (int) Math.sqrt(n);
-        
-        for (int i = 1; i <= sqrtN; i++) {
-            if (n % i == 0) {
-                if (i == n / i) {
-                    cnt++; // i와 n/i가 같은 경우 (즉, n이 완전제곱수)
-                } else {
-                    cnt += 2;
-                }
+            for (int j = 1; j <= number / i; j++) {
+                count[i * j]++;
             }
         }
-        
-        return cnt;
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            if (count[i] > limit) {
+                answer += power;
+            } else {
+                answer += count[i];
+            }
+        }
+        return answer;
     }
 }
