0bd2bd90670c7bd4e0dee14872562fedbedade17
Jihyun Han <startingfrom33@gmail.com>
2025-02-19 23:49:40 +0900

    [level 1] Title: 모의고사, Time: 1.55 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,29 +1,39 @@
 import java.util.*;
 
 class Solution {
-    public static int[] solution(int[] answers) {
-        int[][] patterns = {
-                {1, 2, 3, 4, 5},
-                {2, 1, 2, 3, 2, 4, 2, 5},
-                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
-        };
-
+    public int[] solution(int[] answers) {
         int[] hit = new int[3];
-        for(int i = 0; i < hit.length; i++) {
-            for(int j = 0; j < answers.length; j++) {
-                if(patterns[i][j % patterns[i].length] == answers[j]) hit[i]++;
+        int[][] patterns = {
+            {1, 2, 3, 4, 5}
+            , {2, 1, 2, 3, 2, 4, 2, 5}
+            , {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
+        
+        for (int i = 0; i < hit.length; i++) {
+            for (int j = 0; j < answers.length; j++) {
+                if (patterns[i][j % patterns[i].length] == answers[j]) {
+                    hit[i]++;
+                }
             }
         }
-
+        
         int max = Math.max(hit[0], Math.max(hit[1], hit[2]));
         List<Integer> list = new ArrayList<>();
-        for(int i = 0; i < hit.length; i++)
-            if(max == hit[i]) list.add(i + 1);
-
+        
+        for (int i = 0; i < hit.length; i++) {
+            if (max != hit[i]) {
+                continue;
+            }
+            
+            list.add(i + 1);
+        }
+        
         int[] answer = new int[list.size()];
-        int cnt = 0;
-        for(int num : list)
-            answer[cnt++] = num;
+        
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
         return answer;
     }
 }
