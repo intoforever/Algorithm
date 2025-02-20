e2c23c0d5f0f3ddb5e15e60458b0f66d36ef9046
Jihyun Han <startingfrom33@gmail.com>
2025-02-20 21:40:19 +0900

    [level 1] Title: 과일 장수, Time: 45.63 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -2,35 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
-        int answer = 0;
-        
-        if (m > score.length) {
-            return answer;
-        }
-        
-        int leftOver = score.length % m;
-        Map<Integer, Integer> map = new HashMap<>();
-        
+       int answer = 0;
+
         Arrays.sort(score);
-        
-        for (int i = leftOver, min = score[leftOver]; i <= score.length; i++) {
-            if (i == score.length) {
-                map.put(min, map.getOrDefault(min, 0) + 1);
-                break;
-            }
-            
-            if (i != leftOver && (i - leftOver) % m == 0) {
-                map.put(min, map.getOrDefault(min, 0) + 1);
-                min = score[i];
-            }
-            
-            min = Math.min(min, score[i]);
+
+        for(int i = score.length; i >= m; i -= m){
+            answer += score[i - m] * m;
         }
-        
-        for (Integer key : map.keySet()) {
-            answer += key * m * map.get(key);
-        }
-        
+
         return answer;
     }
 }
