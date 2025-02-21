ea72cbdb0768e52ab92d5a0432a26124785d9910
Jihyun Han <startingfrom33@gmail.com>
2025-02-21 23:19:33 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.17 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] arr = s.split(" ");
+        
+        for (int i = 0; i < arr.length; i++) {
+            if ("Z".equals(arr[i])) {
+                answer -= Integer.parseInt(arr[i - 1]);
+                continue;
+            }
+            
+            answer += Integer.parseInt(arr[i]);
+        }
+        
+        return answer;
+    }
+}
