2869d8eb937f222d3c2670367bb10f56b5882e1d
Jihyun Han <startingfrom33@gmail.com>
2025-02-26 22:00:31 +0900

    [level 1] Title: 소수 만들기, Time: 7.01 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,29 +1,24 @@
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
+       int answer = 0;
 
-        for (int i = 2; i < nums.length; i++) {
-            int num1 = nums[i - 2];
-            for (int j = i; j < nums.length; j++) {
-                int num2 = nums[j - 1];
-                for (int k = j; k < nums.length; k++) {
-                    int num3 = nums[k];
-                    answer += checkIfPrime(num1, num2, num3);
+        for(int i = 0; i < nums.length - 2; i ++){
+            for(int j = i + 1; j < nums.length - 1; j ++){
+                for(int k = j + 1; k < nums.length; k ++ ){
+                    if(isPrime(nums[i] + nums[j] + nums[k])){
+                        answer += 1;  
+                    } 
                 }
             }
         }
-
         return answer;
     }
     
-    public int checkIfPrime(int num1, int num2, int num3) {
-        boolean isPrime = true;
-        int num = num1 + num2 + num3;
-        for (int i = 2; i < Math.sqrt(num) + 1; i++) {
-            if (num % i == 0) {
-                return 0;
-            }
+    public Boolean isPrime(int num){
+        int cnt = 0;
+        for(int i = 1; i <= (int)Math.sqrt(num); i ++){
+            if(num % i == 0) cnt += 1; 
         }
-        return 1;
+        return cnt == 1;
     }
 }
