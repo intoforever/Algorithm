5355a20966444b9eb11a5f6278ae328699ca7112
Jihyun Han <startingfrom33@gmail.com>
2025-02-28 00:32:40 +0900

    [level 0] Title: 배열 만들기 6, Time: 57.79 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -2,18 +2,20 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-        Deque<Integer> stk = new ArrayDeque<>();
+        Stack<Integer> stk = new Stack<>();
         
-        for (int i = 0; i < arr.length; i++) {
-            if (stk.size() == 0 || stk.peekLast() != arr[i]) {
-                stk.offerLast(arr[i]);
-                continue;
+        for (int a : arr) {
+            if (stk.size() == 0) {
+                stk.push(a);
+            } else if (stk.peek() == a) {
+                stk.pop();
+            } else {
+                stk.push(a);
             }
-            stk.pollLast();
-            
-            if (i == arr.length - 1 && stk.size() == 0) {
-                stk.offerLast(-1);
-            }
+        }
+        
+        if (stk.size() == 0) {
+            stk.push(-1);
         }
         
         return stk.stream().mapToInt(Integer::intValue).toArray();
