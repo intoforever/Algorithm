5bca0e1c64fcabdb7fb30552983270fb8dc8eaa6
Jihyun Han <startingfrom33@gmail.com>
2025-03-03 19:32:45 +0900

    [level 1] Title: 실패율, Time: 5.90 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,49 +1,52 @@
 import java.util.*;
 
 class Solution {
+    // 스테이지 번호와 실패율(혹은 원하는 값을 저장하는 클래스)
+    static class StageInfo {
+        int stage;
+        double failureRate;
+        
+        StageInfo(int stage, double failureRate) {
+            this.stage = stage;
+            this.failureRate = failureRate;
+        }
+    }
+    
     public int[] solution(int N, int[] stages) {
-        double[] arr = new double[N + 1];
+        int totalPlayers = stages.length;
+        // 각 스테이지별 도전한 플레이어 수(스테이지 N+1은 클리어한 플레이어이므로 카운트하지 않음)
+        int[] stageCount = new int[N + 2];
         for (int s : stages) {
-            if (s == N + 1) {
-                continue;
-            }
-            arr[s]++;
-        }
-
-        List<Integer> list1 = new LinkedList<>(); // 0 아닌 애들 idx 리스트
-        List<Integer> list2 = new LinkedList<>(); // 0인 애들 idx 리스트
-        int len = stages.length;
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] == 0.0) {
-                list2.add(i);
-                continue;
-            }
-            int val = (int) arr[i];
-            arr[i] /= len;
-            len -= val;
-            list1.add(i);
+            stageCount[s]++;
         }
         
-        List<Integer> list3 = new LinkedList<>(); // 0 아닌 애들 idx 리스트 정렬
-        double[] arr2 = Arrays.copyOf(arr, arr.length);
-        Arrays.sort(arr2);
-        int idx = arr.length - 1;
-        while (arr2[idx] != 0) {
-            for (int i = 0; i < list1.size(); i++) {
-                int arrIdx = list1.get(i);
-                if (arr[arrIdx] == arr2[idx]) {
-                    list3.add(arrIdx);
-                    list1.remove(i);
-                    idx--;
-                    break;
-                }
+        StageInfo[] stageInfos = new StageInfo[N];
+        int remaining = totalPlayers;
+        // 1번 스테이지부터 N번 스테이지까지 실패율 계산
+        for (int i = 1; i <= N; i++) {
+            double failureRate = 0;
+            if (remaining > 0) {
+                failureRate = (double) stageCount[i] / remaining;
             }
+            stageInfos[i - 1] = new StageInfo(i, failureRate);
+            remaining -= stageCount[i];
         }
         
-        List<Integer> list4 = new LinkedList<>();
-        list4.addAll(list3);
-        list4.addAll(list2);
+        // 실패율 내림차순, 실패율이 같다면 스테이지 번호 오름차순으로 정렬
+        Arrays.sort(stageInfos, new Comparator<StageInfo>() {
+            @Override
+            public int compare(StageInfo a, StageInfo b) {
+                if (b.failureRate > a.failureRate) return 1;
+                else if (b.failureRate < a.failureRate) return -1;
+                else return Integer.compare(a.stage, b.stage);
+            }
+        });
         
-        return list4.stream().mapToInt(Integer::intValue).toArray();
+        // 정렬된 순서대로 스테이지 번호를 배열에 담아서 반환
+        int[] answer = new int[N];
+        for (int i = 0; i < N; i++) {
+            answer[i] = stageInfos[i].stage;
+        }
+        return answer;
     }
 }
