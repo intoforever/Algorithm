ebb11886f1bb954fd0a53d961113c2affbdc8577
Jihyun Han <startingfrom33@gmail.com>
2025-03-08 13:59:42 +0900

    [level 0] Title: 두 수의 합, Time: 13.44 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -1,39 +1,17 @@
 class Solution {
     public String solution(String a, String b) {
-        String answer;
-        if (a.length() >= b.length()) {
-            answer = getSumValue(a, b);
-        } else {
-            answer = getSumValue(b, a);
-        }
-        return answer;
-    }
-    
-    private String getSumValue(String num1, String num2) {
         StringBuilder sb = new StringBuilder();
-        int flag = 0, len1 = num1.length(), len2 = num2.length(), gap = Math.abs(len1 - len2);
-        
-        if (gap > 0) {
-            for (int i = 0; i < gap; i++) {
-                sb.append(0);
-            }
+        int carry = 0;
+        int i = a.length() - 1, j = b.length() - 1;
 
-            sb.append(num2);
-            num2 = sb.toString();
-            sb.setLength(0);
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            sb.append(sum % 10);
+            carry = sum / 10;
         }
-        
-        for (int i = num2.length() - 1; i >= 0; i--) {
-            int val1 = num1.charAt(i) - '0';
-            int val2 = num2.charAt(i) - '0';
-            sb.append((val1 + val2 + flag) % 10);
-            flag = ((val1 + val2 + flag) / 10);
-        }
-        
-        if (flag == 1) {
-            sb.append(flag);
-        }
-        
         return sb.reverse().toString();
     }
 }
