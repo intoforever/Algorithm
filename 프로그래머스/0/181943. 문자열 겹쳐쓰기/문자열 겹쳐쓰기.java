2e62f879ad1bb8b6b7f531ac3e2be6b4e075e1d7
Jihyun Han <startingfrom33@gmail.com>
2025-03-16 21:50:21 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 13.60 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,8 +1,8 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        StringBuilder sb = new StringBuilder(my_string);
-        int e = s + overwrite_string.length();
-        sb.replace(s, e, overwrite_string);
-        return sb.toString();
+        String prefix = my_string.substring(0, s);
+        String suffix = my_string.substring(s + overwrite_string.length());
+
+        return prefix + overwrite_string + suffix;
     }
 }
