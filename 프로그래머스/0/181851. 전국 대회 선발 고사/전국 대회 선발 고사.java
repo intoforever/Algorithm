92cf607dde052c0fa9f2424a931647818e115ba8
Jihyun Han <startingfrom33@gmail.com>
2025-03-19 00:01:22 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 1.66 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -2,31 +2,41 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
-        int answer = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < rank.length; i++) {
-            map.put(rank[i], i);
+         int answer = 0;
+
+        Student[] student = new Student[rank.length];
+
+        for(int i=0; i<rank.length; i++) {
+            student[i] = new Student(i, rank[i], attendance[i]);
         }
-        
-        Arrays.sort(rank);
-        int delimiter = 10000;
-        int idx;
-        int cnt = 1;
-        
-        for (int i = 0; i < rank.length; i++) {
-            idx = map.get(rank[i]);
-            if (attendance[idx]) {
-                answer += idx * delimiter;
-                delimiter /= 100;
-                cnt++;
-            }
-            
-            if (cnt > 3) {
-                break;
+
+        Arrays.sort(student, (o1, o2) -> o1.rank - o2.rank);
+
+        int cnt = 0;
+        int[] abc = new int[3];
+
+        for(int i=0; i<rank.length; i++) {
+            if(cnt == 3) break;
+
+            if(student[i].attendance) {
+                abc[cnt++] = student[i].num;     
             }
         }
-        
+
+        answer = 10000 * abc[0] + 100 * abc[1] + abc[2];
+
         return answer;
+    }
+
+    class Student {
+        int num;
+        int rank;
+        boolean attendance;
+
+        Student(int num, int rank, boolean attendance) {
+            this.num = num;
+            this.rank = rank;
+            this.attendance = attendance;
+        }
     }
 }
