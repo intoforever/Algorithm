f0418e6ff0a69a907774361e0384a1373b48a3fd
Jihyun Han <startingfrom33@gmail.com>
2025-03-20 06:46:16 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,18 +1,18 @@
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
-        String answer = "fail";
         String userId = id_pw[0], userPw = id_pw[1];
         
         for (String[] info : db) {
             String id = info[0], pw = info[1];
             
-            if (id.equals(userId) && pw.equals(userPw)) {
-                return "login";
-            } else if (id.equals(userId)) {
+            if (id.equals(userId)) {
+                if (pw.equals(userPw)) {
+                    return "login";
+                }
                 return "wrong pw";
             }
         }
         
-        return answer;
+        return "fail";
     }
 }
