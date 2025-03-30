cf59e24a3f4361f8baa1eaaaaa3380fd01d4db94
Jihyun Han <startingfrom33@gmail.com>
2025-03-30 15:17:01 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -1,18 +1,5 @@
 class Solution {
     public int solution(int M, int N) {
-        int answer = 0;
-        if (M == 1 && N == 1) {
-            return answer;
-        }
-        
-        int bigger = Math.max(M, N);
-        int shorter = Math.min(M, N);
-        
-        answer = shorter - 1;
-        for (int i = 0; i < shorter; i++) {
-            answer += bigger - 1;
-        }
-        
-        return answer;
+        return M*N-1;
     }
 }
