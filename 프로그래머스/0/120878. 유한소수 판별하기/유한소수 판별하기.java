7e34c2ca778c8abdc35e4174583ea66745bd0ab1
Jihyun Han <startingfrom33@gmail.com>
2025-03-31 21:08:20 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -2,42 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int a, int b) {
-        int answer = 2;
-        int[] aDivisor = getDivisor(a);
-        
-        for (int val : aDivisor) {
-            if (b % val == 0) {
-                b /= val;
-            }
-        }
-        
-        while (b % 2 == 0 || b % 5 == 0) {
-            if (b % 2 == 0) {
-                b /= 2;
-            }
+        b /= gcd(a, b);
+        while (b != 1) {
             if (b % 5 == 0) {
                 b /= 5;
-            }
-        }
-        
-        if (b == 1) {
-            answer = 1;
-        }
-        
-        return answer;
-    }
-    
-    public int[] getDivisor(int a) {
-        Set<Integer> set = new HashSet<>();
-        for (int i = 2; i <= a; i++) {
-            if (a % i != 0) {
                 continue;
             }
-            set.add(i);
-            while (a % i == 0) {
-                a /= i;
+            if (b % 2 == 0) {
+                b /= 2;
+                continue;
             }
+            return 2;
         }
-        return set.stream().mapToInt(Integer::intValue).toArray();
+        return 1;
+    }
+
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
     }
 }
