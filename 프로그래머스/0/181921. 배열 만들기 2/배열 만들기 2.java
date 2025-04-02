330137ed5a8ae2001ebb6e1ce62565a1978f72a1
Jihyun Han <startingfrom33@gmail.com>
2025-04-02 22:00:15 +0900

    [level 0] Title: 배열 만들기 2, Time: 2.79 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -2,25 +2,20 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int l, int r) {
-        // 이진법 활용
-        // l = 5, r = 555
-        // 5,   50,   55,   500,   505,   550,   555
-        // 1    10    11    100    101    110    111
-        // 1     2     3      4      5      6      7
-
-        // 지수는 자릿수
-        // 2^0 <= x < 2^3
-        int digit = String.valueOf(r).length();
-        int exponent = (int)Math.pow(2.0, digit);
-
+        int[] answer = {};
+        // binaryString() 이용해서 다시 풀어보기
+        // 1 10 11 100 101 110 111
         List<Integer> list = new ArrayList<>();
+        int cnt = String.valueOf(r).length();
+        int exponent = (int) Math.pow(2, cnt);
+        
         for (int i = 0; i < exponent; i++) {
-            String binaryString = Integer.toBinaryString(i);
-            int target = Integer.parseInt(binaryString) * 5;
-            if (target >= l && target <= r)
+            int target = Integer.parseInt(Integer.toBinaryString(i)) * 5;
+            if (l <= target && target <= r) {
                 list.add(target);
+            }
         }
-
-        return list.isEmpty() ? new int[] { -1 } : list.stream().mapToInt(i -> i).toArray();
+        
+        return list.isEmpty() ? new int[]{-1} : list.stream().mapToInt(i->i).toArray();
     }
 }
