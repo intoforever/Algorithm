78a3888d689e79eee4b4e97b014efffaf1f8a03b
Jihyun Han <startingfrom33@gmail.com>
2025-04-05 22:22:38 +0900

    [level 0] Title: 특이한 정렬, Time: 0.18 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -2,30 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numlist, int n) {
-        Map<Integer, String> map = new HashMap<>();
-        StringBuilder sb = new StringBuilder();
-        Arrays.sort(numlist);
-        
-        for (int num : numlist) {
-            int val = Math.abs(n - num);
-            sb.append(num);
-            sb.append("-");
-            map.put(val, map.getOrDefault(val, "") + sb.toString());
-            sb.setLength(0);
-        }
-        
-        int[] answer = new int[numlist.length];
-        int[] keys = map.keySet().stream().mapToInt(i->i).toArray();
-        Arrays.sort(keys);
-        int idx = 0;
-        for (int key : keys) {
-            String[] arr = map.get(key).split("-");
-            for (int j = arr.length - 1; j >= 0; j--) {
-                int val = Integer.parseInt(arr[j]);
-                answer[idx++] = val;
+        int size = numlist.length;
+        for(int i=0; i<size-1; i++){
+            for(int k=i+1; k<size; k++){
+                int a = (numlist[i] - n) * (numlist[i] > n ? 1 : -1);
+                int b = (numlist[k] - n) * (numlist[k] > n ? 1 : -1);
+                if(a > b || (a == b && numlist[i] < numlist[k])){
+                    int temp = numlist[i];
+                    numlist[i] = numlist[k];
+                    numlist[k] = temp;
+                }
+
             }
         }
-        
-        return answer;
+        return numlist;
     }
 }
