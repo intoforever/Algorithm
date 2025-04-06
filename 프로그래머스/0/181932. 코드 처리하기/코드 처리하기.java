135ebc7aa553cc7a7e8ced2b2b05157d435faf06
Jihyun Han <startingfrom33@gmail.com>
2025-04-06 21:17:50 +0900

    [level 0] Title: 코드 처리하기, Time: 7.14 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,15 +1,15 @@
 class Solution {
     public String solution(String code) {
         StringBuilder sb = new StringBuilder();
-        boolean mode = false;
+        int mode = 0;
         
         for (int i = 0; i < code.length(); i++) {
             char c = code.charAt(i);
             if (c == '1') {
-                mode = !mode;
+                mode = mode == 0 ? 1 : 0;
                 continue;
             }
-            if ((!mode && i % 2 == 0) || (mode && i % 2 == 1)) {
+            if (i % 2 == mode) {
                 sb.append(c);    
             }
         }
