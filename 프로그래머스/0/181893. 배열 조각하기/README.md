624d4141d3fbd045cb99623946888d8632f26a12
Jihyun Han <startingfrom33@gmail.com>
2025-04-14 22:56:51 +0900

    [level 0] Title: 배열 조각하기, Time: 0.04 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/README.md
+++ 프로그래머스/0/181893. 배열 조각하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 조각하기 - 181893 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181893#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181893) 
 
 ### 성능 요약
 
-메모리: 80.7 MB, 시간: 5.39 ms
+메모리: 76.3 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 14일 22:52:51
+2025년 04월 14일 22:56:49
 
 ### 문제 설명
 
--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -1,48 +1,16 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] arr, int[] query) {
-        MyArr myArr = new MyArr(Arrays.copyOf(arr, arr.length));
-        int[] answer = new int[0];
+        int start = 0;
+        int end = arr.length - 1;
         for (int i = 0; i < query.length; i++) {
-            int idx = query[i];
-            boolean isEven = (i % 2 == 0);
-            answer = myArr.getNewArr(idx, isEven);
+            if (i % 2 == 0) {
+                end = start + query[i] - 1;
+            } else {
+                start += query[i];
+            }
         }
-        return answer;
-    }
-}
 
-class MyArr {
-    private int[] arr;
-    
-    public MyArr(int[] arr) {
-        this.arr = arr;
-    }
-    
-    public int[] getNewArr(int idx, boolean isEven) {
-        return isEven ? getEvenIdxArr(idx) : getOddIdxArr(idx);
-    }
-    
-    private int[] getOddIdxArr(int idx) {
-        int len = arr.length - idx;
-        int[] temp = new int[len];
-        
-        for (int i = idx, z = 0; i < arr.length; i++, z++) {
-            temp[z] = arr[i];
-        }
-        arr = temp;
-        return arr;
-    }
-    
-    private int[] getEvenIdxArr(int idx) {
-        int len = idx + 1;
-        int[] temp = new int[len];
-        
-        for (int i = 0, z = 0; i < len; i++, z++) {
-            temp[z] = arr[i];
-        }
-        arr = temp;
-        return arr;
+        return Arrays.copyOfRange(arr, start, end + 2);
     }
 }
