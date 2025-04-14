ede878b0762acab198054ce6dc0a4816c3d746ce
Jihyun Han <startingfrom33@gmail.com>
2025-04-14 22:59:48 +0900

    [level 0] Title: 배열 조각하기, Time: 0.04 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -5,12 +5,12 @@ class Solution {
         int end = arr.length - 1;
         for (int i = 0; i < query.length; i++) {
             if (i % 2 == 0) {
-                end = start + query[i] - 1;
+                end = start + query[i];
             } else {
                 start += query[i];
             }
         }
 
-        return Arrays.copyOfRange(arr, start, end + 2);
+        return Arrays.copyOfRange(arr, start, end + 1);
     }
 }
