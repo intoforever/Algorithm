3bcb741739c5786f9e817175f784d4c639c573f1
Jihyun Han <startingfrom33@gmail.com>
2025-05-30 01:03:44 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.05 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,67 +1,30 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        Lotto lot = new Lotto(lottos, win_nums);
-        return new int[]{lot.getMaxRank(), lot.getMinRank()};
-    }
-    
-    static class Lotto {
-        private int[] lottos;
-        private int[] winLottos;
-        
-        public Lotto(int[] lottos, int[] winLottos) {
-            this.lottos = lottos;
-            this.winLottos = winLottos;
-        }
-        
-        private List<Integer> getMyLottoNum() {
-            List<Integer> list = new ArrayList<>();
-            for (int lotto : lottos) {
-                if (lotto == 0) {
-                    continue;
-                }
-                list.add(lotto);
+        Map<Integer, Boolean> map = new HashMap<Integer, Boolean>();
+        int zeroCount = 0;
+
+        for(int lotto : lottos) {
+            if(lotto == 0) {
+                zeroCount++;
+                continue;
             }
-            return list;
+            map.put(lotto, true);
         }
-        
-        private int cntZero() {
-            int zeroCnt = 0;
-            
-            Arrays.sort(lottos);
-            for (int lotto : lottos) {
-                if (lotto != 0) {
-                    break;
-                }
-                zeroCnt++;
-            }
-            
-            return zeroCnt;
+
+
+        int sameCount = 0;
+        for(int winNum : win_nums) {
+            if(map.containsKey(winNum)) sameCount++;
         }
-        
-        private int cntEqualNums() {
-            List<Integer> lottoNumList = getMyLottoNum();
-            int cnt = 0;
-            for (Integer num : lottoNumList) {
-                for (int i = 0; i < winLottos.length; i++) {
-                    if (num == winLottos[i]) {
-                        cnt++;
-                        break;
-                    }
-                }
-            }
-            return cnt;
-        }
-        
-        public int getMaxRank() {
-            int max = cntEqualNums() + cntZero();
-            return 7 - max == 7 ? 6 : 7 - max;
-        }
-        
-        public int getMinRank() {
-            int min = cntEqualNums();
-            return 7 - min == 7 ? 6 : 7 - min;
-        }
+
+        int maxRank = 7 - (sameCount + zeroCount);
+        int minRank = 7 - sameCount;
+        if(maxRank > 6) maxRank = 6;
+        if(minRank > 6) minRank = 6;
+
+        return new int[] {maxRank, minRank};
     }
 }
