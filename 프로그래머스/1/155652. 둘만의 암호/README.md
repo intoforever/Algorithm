a34f24ff25da234d1860d1cf08c212c7ed0b90a1
Jihyun Han <startingfrom33@gmail.com>
2025-06-26 21:05:37 +0900

    [level 1] Title: 둘만의 암호, Time: 1.19 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652) 
 
 ### 성능 요약
 
-메모리: 74.2 MB, 시간: 0.40 ms
+메모리: 80.5 MB, 시간: 1.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 26일 20:59:41
+2025년 06월 26일 21:05:34
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,37 +1,19 @@
-import java.util.*;
-
 class Solution {
     public String solution(String s, String skip, int index) {
-        StringBuilder sb = new StringBuilder();
-        Map<Character, Integer> map = new HashMap<>();
-        List<Character> list = new ArrayList<>();
-        init(map, list, skip);
-        
-        // s에 있는 문자열 순회하면서 pool의 hash
-        char[] arr = s.toCharArray();
-        for (int i = 0; i < arr.length; i++) {
-            int value = map.get(arr[i]) + index;
-            sb.append(list.get(value % list.size()));
-        }
-        
-        return sb.toString();
-    }
-    
-    public void init(Map<Character, Integer> map, List<Character> list, String skip) {
-        char[] charArr = skip.toCharArray();
-        Set<Character> set = new HashSet<>();
-        for (char c : charArr) {
-            set.add(c);
-        }
-        
-        char c = 97; // 'a'
-        int idx = 0;
-        for (int i = 0; i < 26; i++, c++) {
-            if (set.contains(c)) {
-                continue;
+       StringBuilder answer = new StringBuilder();
+
+        for (char letter : s.toCharArray()) {
+            char temp = letter;
+            int idx = 0;
+            while (idx < index) {
+                temp = temp == 'z' ? 'a' : (char) (temp + 1);
+                if (!skip.contains(String.valueOf(temp))) {
+                    idx += 1;
+                }
             }
-            map.put(c, idx++);
-            list.add(c);
+            answer.append(temp);
         }
+
+        return answer.toString();
     }
 }
