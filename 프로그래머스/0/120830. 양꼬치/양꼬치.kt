d1654bc25c9cd72f0dc61a988ad0a06154420e0b
Jihyun Han <startingfrom33@gmail.com>
2025-07-02 00:04:26 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.kt
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.kt
@@ -1,7 +1,3 @@
 class Solution {
-    fun solution(n: Int, k: Int): Int {
-        val lamb: Int = n * 12000
-        val drinks: Int = (k - (n / 10)) * 2000
-        return lamb + drinks
-    }
+    fun solution(n: Int, k: Int): Int = (n * 12000) + ((k - (n / 10)) * 2000)
 }
