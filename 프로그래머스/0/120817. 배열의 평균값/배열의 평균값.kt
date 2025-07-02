655bc3daf51b836a5535ddb2c4e7e68837a8694b
Jihyun Han <startingfrom33@gmail.com>
2025-07-02 23:59:35 +0900

    [level 0] Title: 배열의 평균값, Time: 0.07 ms, Memory: 63.9 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.kt
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.kt
@@ -1,3 +1,12 @@
 class Solution {
-    fun solution(numbers: IntArray): Double = numbers.average()
+    fun solution(numbers: IntArray): Double {
+        if (numbers.size == 0) {
+            return 0.0
+        }
+        var sum = 0
+        for (num in numbers) {
+            sum += num
+        }
+        return sum.toDouble() / numbers.size
+    }
 }
