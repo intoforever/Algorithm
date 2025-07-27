cb841485c426a99405ab611375d69e383f0dd400
Jihyun Han <startingfrom33@gmail.com>
2025-07-28 00:03:27 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.kt
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.kt
@@ -1,17 +1,3 @@
 class Solution {
-    fun solution(num_list: IntArray): IntArray {
-        var odd: Int = 0
-        var even: Int = 0
-        
-        for (i in 0..num_list.size - 1) {
-            val num = num_list[i]
-            if (num % 2 == 0) {
-                even++
-            } else {
-                odd++
-            }
-        }
-        
-        return intArrayOf(even, odd)
-    }
+    fun solution(num_list: IntArray) = intArrayOf(num_list.count{ it % 2 == 0 }, num_list.count{ it % 2 == 1 })
 }
