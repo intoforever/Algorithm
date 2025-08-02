8ef2356e75924a391beaf0c4b9cc2d3d9bece7ad
Jihyun Han <startingfrom33@gmail.com>
2025-08-02 22:55:21 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 65 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.kt
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.kt
@@ -1,16 +1,3 @@
 class Solution {
-    fun solution(slice: Int, n: Int): Int {
-        var answer = 1
-        var flag = true
-        
-        while (flag) {
-            if ((slice * answer) / n >= 1) {
-                flag = false
-                break
-            }
-            answer++
-        }
-        
-        return answer
-    }
+    fun solution(slice: Int, n: Int) = n / slice + if (n % slice > 0) 1 else 0
 }
